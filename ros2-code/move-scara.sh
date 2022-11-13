10d58ea208ec5929c9989b96c4ad314f40791e80
Arjan Gupta <arjangupta95@gmail.com>
2022-11-13 12:14:25 -0600

    Add full configuration for tool joint and tool link

--- ros2-code/move-scara.sh
+++ ros2-code/move-scara.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ros2 topic pub --once /forward_position_controller/commands std_msgs/msg/Float64MultiArray "{data: [$1, $2]}"
+ros2 topic pub --once /forward_position_controller/commands std_msgs/msg/Float64MultiArray "{data: [$1, $2, $3]}"
