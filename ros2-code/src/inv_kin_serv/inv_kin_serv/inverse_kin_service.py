4b352403e3a91aa7628bf99a5ad4d0bf91cbd96d
Arjan Gupta <arjangupta95@gmail.com>
2022-11-15 23:58:26 -0600

    Attempt to import a geometry message - needs to be replaced by a custom interface message

--- ros2-code/src/inv_kin_serv/inv_kin_serv/inverse_kin_service.py
+++ ros2-code/src/inv_kin_serv/inv_kin_serv/inverse_kin_service.py
@@ -1,17 +1,16 @@
 import rclpy
 from rclpy.node import Node
-from example_interfaces.srv import AddTwoInts
+from geometry_msgs.msg import Point
 
 # Service Node for inverse kinematics
 class InverseKinService(Node):
 
     def __init__(self):
         super().__init__('inverse_kin_service')
-        self.srv = self.create_service(AddTwoInts, 'add_two_ints', self.calculate_inverse_kin)
+        self.srv = self.create_service(Point, 'inverse_kin_service', self.calculate_inverse_kin)
 
     def calculate_inverse_kin(self, request, response):
-        response.sum = request.a + request.b
-        self.get_logger().info('Incoming request\na: %d b: %d' % (request.a, request.b))
+        sum = request.x + request.y + request.z # this won't work
 
         return response
 
