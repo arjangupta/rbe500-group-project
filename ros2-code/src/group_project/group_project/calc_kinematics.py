7062db550ed6b7bd55671bbe8245fc2d5dd00781
Joshua Gross <jjgross@wpi.edu>
2022-11-16 17:12:55 -0500

    newest and greatest forward kinematics (working)

--- ros2-code/src/group_project/group_project/calc_kinematics.py
+++ ros2-code/src/group_project/group_project/calc_kinematics.py
@@ -17,16 +17,12 @@ class Subscriber(Node):
     def __init__(self):
 
         super().__init__('Subscriber')
-        # Declare the link lengths so that they may be accessed for the inverse and forward kinematics
         self.p1=2
         self.p2=1
         self.p3=1
 
-        # Create subscriebr to listen for the joint values array to be published to the JointValues topic
         self.listening = self.create_subscription(Float32MultiArray, 'JointInputs', self.calculate_forward_kinematics, 10)  
-
-        # Create subscriber to listen for the end effector pose to be published to the EndEffector topic
-        #self.listening = self.create_subscription(Pose, 'EndEffectorLoc', self.calculate_inverse_kinematics, 10)
+        self.publishing = self.create_publisher(Pose, 'EndEffectorPose', 10)
 
     def calculate_forward_kinematics(self, mesgfrompub):
 
@@ -40,15 +36,12 @@ class Subscriber(Node):
 
         link_lengths = [self.p1, self.p2, self.p3]
 
-
-
         T30 = np.array([[((cos(theta1)*cos(theta2))-(sin(theta1)*sin(theta2))), ((-cos(theta1)*sin(theta2))-(cos(theta2)*sin(theta1))), 0, ((self.p2*cos(theta1)) + (self.p3*cos(theta1)*cos(theta2)) - (self.p3*sin(theta1)*sin(theta2)))],
         [((cos(theta1)*sin(theta2)) + (cos(theta2)*sin(theta1))), ((cos(theta1)*cos(theta2)) - (sin(theta1)*sin(theta1))), 0, ((self.p2*sin(theta1)) + (self.p3*cos(theta1)*sin(theta2)) + (self.p3*cos(theta2)*sin(theta1)))],
         [0, 0, 1, (self.p1 + disp3)],
         [0, 0, 0, 1]])
 
         rotation_matrix = np.array([[T30[0][0], T30[0][1], T30[0][2]], [T30[1][0], T30[1][1], T30[1][2]], [T30[2][0], T30[2][1], T30[2][2]]])
-        # rot = R.from_matrix(rotation_matrix)
         quaternion = R.from_matrix(rotation_matrix).as_quat()
     
         pose = Pose()
@@ -59,10 +52,7 @@ class Subscriber(Node):
         pose.orientation.y = quaternion[1]
         pose.orientation.z = quaternion[2]
         pose.orientation.w = quaternion[3]
-
-        #print(p)
-
-        self.publishing = self.create_publisher(Pose, 'JointInputs', 10)
+ 
         self.publishing.publish(pose)
         print(pose)
 
