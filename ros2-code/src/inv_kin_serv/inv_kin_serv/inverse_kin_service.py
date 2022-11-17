4a608befbe6800e034a0d03156998f448c6fa58c
Arjan Gupta <arjangupta95@gmail.com>
2022-11-16 22:24:08 -0600

    Establish basic server-client comms

--- ros2-code/src/inv_kin_serv/inv_kin_serv/inverse_kin_service.py
+++ ros2-code/src/inv_kin_serv/inv_kin_serv/inverse_kin_service.py
@@ -1,16 +1,22 @@
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import Point
+from rbe500_custom_interfaces.srv import InvKinSCARA
 
 # Service Node for inverse kinematics
 class InverseKinService(Node):
 
     def __init__(self):
         super().__init__('inverse_kin_service')
-        self.srv = self.create_service(Point, 'inverse_kin_service', self.calculate_inverse_kin)
+        self.srv = self.create_service(InvKinSCARA, 'inverse_kin_service', self.calculate_inverse_kin)
 
     def calculate_inverse_kin(self, request, response):
-        sum = request.x + request.y + request.z # this won't work
+        # Log to terminal that position was received
+        print(f"We received an end effector position of x:{request.x} y:{request.y} z:{request.z}")
+
+        # Calculate joint variables
+        response.q1 = float(1.0)
+        response.q2 = float(2.0)
+        response.q3 = float(3.0)
 
         return response
 
