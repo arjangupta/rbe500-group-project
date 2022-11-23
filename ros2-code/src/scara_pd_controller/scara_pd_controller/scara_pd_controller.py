858bd5d9c6a85e2a3b9003bac0608df64b617465
Arjan Gupta <arjangupta95@gmail.com>
2022-11-23 14:25:17 -0600

    Create publisher and describe it

--- ros2-code/src/scara_pd_controller/scara_pd_controller/scara_pd_controller.py
+++ ros2-code/src/scara_pd_controller/scara_pd_controller/scara_pd_controller.py
@@ -5,6 +5,7 @@ import rclpy
 from rclpy.node import Node
 from rbe500_custom_interfaces.srv import ScaraRefPos
 from sensor_msgs.msg import JointState
+from std_msgs.msg import Float64MultiArray
 
 # Node for subscriber, publisher, service, and client
 class ScaraPDController(Node):
@@ -29,16 +30,33 @@ class ScaraPDController(Node):
         # Create the subscriber that receives the joint state information, with a queue of 50 since it is very fast
         self.subscription = self.create_subscription(JointState, '/joint_states', self.joint_states_callback, 100)
         print("Done creating subscription that receives joint-state information.")
+
+        # Create the publisher that will send the joint efforts
+        self.publisher = self.create_publisher(Float64MultiArray, '/forward_effort_controller/commands', 10)
+        print("Done creating the publisher for sending joint efforts.")
     
-    def joint_states_callback(self, joint_state_message):
+    def joint_states_callback(self, joint_state_msg):
         # Only take action if we have a reference/goal position to work against
         if self.received_ref_pos:
-            print("We're ready to start our controller!")
+            self.run_controller(joint_state_msg)
         elif self.awaiting_ref_pos_count >= 100:
             print("Receiving joint state info. Awaiting a reference position to be given.")
             self.awaiting_ref_pos_count = 0
         else:
             self.awaiting_ref_pos_count += 1
+
+    def run_controller(self, joint_state_msg):
+        # Get joint position values
+        q1 = joint_state_msg.position[0]
+        q2 = joint_state_msg.position[1]
+        q3 = joint_state_msg.position[2]
+        # Get joint velocity values
+        v1 = joint_state_msg.velocity[0]
+        v2 = joint_state_msg.velocity[0]
+        v3 = joint_state_msg.velocity[0]
+        print("Controller is running!")
+        print(f"Current q values are q1:{q1}, q2:{q2}, q3:{q3}")
+        print(f"Current joint velocities are v1:{v1}, v2:{v2}, v3:{v3}")
     
     def set_ref(self, request, response):
         # Log to terminal that reference/goal position was received
@@ -51,6 +69,7 @@ class ScaraPDController(Node):
 
         # Set flag that we've received the goal position
         self.received_ref_pos = True
+        print("We're ready to start our controller!")
 
         # Return the acknowledgement
         response.ok = True
