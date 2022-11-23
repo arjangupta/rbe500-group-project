8c10d96295db418aa86275bf60bb6468c376849c
Arjan Gupta <arjangupta95@gmail.com>
2022-11-23 02:08:42 -0600

    Import the custom service interface

--- ros2-code/src/scara_pd_controller/scara_pd_controller/scara_pd_controller.py
+++ ros2-code/src/scara_pd_controller/scara_pd_controller/scara_pd_controller.py
@@ -3,7 +3,7 @@
 
 import rclpy
 from rclpy.node import Node
-from rbe500_custom_interfaces.srv import InvKinSCARA
+from rbe500_custom_interfaces.srv import ScaraRefPos
 
 # Node for subscriber, publisher, service, and client
 class ScaraPDController(Node):
@@ -12,8 +12,15 @@ class ScaraPDController(Node):
         # Initialize superclass
         super().__init__('scara_pd_controller')
 
+        # Initialize member variables
+        self.received_ref_pos: bool = False
+
         # Create the service
-        self.srv = self.create_service(InvKinSCARA, 'scara_pd_controller', self.calculate_inverse_kin)
+        self.srv = self.create_service(ScaraRefPos, 'scara_pd_controller', self.set_ref)
+    
+    def set_ref(self, request, response):
+        # Log to terminal that goal position was received
+        print(f"We received an reference position of x:{request.x} y:{request.y} z:{request.z}")
 
 
 def main():
