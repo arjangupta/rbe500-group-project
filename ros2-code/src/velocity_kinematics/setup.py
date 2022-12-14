217dbc8d148b81ff4b7fbe79967064ac8fb390e4
Arjan Gupta <arjangupta95@gmail.com>
2022-12-13 19:37:38 -0600

    Create velocity kinematics node

---
+++ ros2-code/src/velocity_kinematics/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'velocity_kinematics'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='arjan',
+    maintainer_email='agupta11@wpi.edu',
+    description='ROS Node for Velocity Kinematics Calculations',
+    license='Property of WPI',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
